5b7f7a01f8f770e0bcd8bf544d9f189611d416c5
Aimbe <qkr2435@gmail.com>
2024-12-26 00:58:02 +0900

    refactor(review): 확장함수 이용하여 depth 분리

--- src/main/kotlin/minesweeper/domain/BoardDisplayFactory.kt
+++ src/main/kotlin/minesweeper/domain/BoardDisplayFactory.kt
@@ -4,12 +4,16 @@ object BoardDisplayFactory {
     fun present(board: Board): String {
         return buildString {
             for (row in 0 until board.size.rows) {
-                for (col in 0 until board.size.columns) {
-                    if (col > 0) append(" ")
-                    append(cellRepresentation(board, Position(row, col)))
-                }
+                appendRow(board, row)
                 if (row < board.size.rows - 1) append("\n")
             }
+        }
+    }
+
+    private fun StringBuilder.appendRow(board: Board, row: Int) {
+        for (col in 0 until board.size.columns) {
+            if (col > 0) append(" ")
+            append(cellRepresentation(board, Position(row, col)))
         }
     }
 
