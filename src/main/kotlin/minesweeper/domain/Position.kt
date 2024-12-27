ee8117c6309461ca5fb73bd53ded9165a38ba71f
Aimbe <qkr2435@gmail.com>
2024-12-27 18:22:36 +0900

    refactor: 불필요 메소드 제거

--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -5,13 +5,4 @@ data class Position(val row: Int, val column: Int) {
         require(row >= 0) { "행은 0 이상이어야 합니다." }
         require(column >= 0) { "열은 0 이상이어야 합니다." }
     }
-
-    companion object {
-        fun random(boardSize: BoardSize): Position {
-            return Position(
-                row = (0 until boardSize.rows).random(),
-                column = (0 until boardSize.columns).random(),
-            )
-        }
-    }
 }
