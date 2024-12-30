c8a527fb34a894bb0ce80bc64e6b1e9e5f9d46cc
Aimbe <qkr2435@gmail.com>
2024-12-30 18:09:42 +0900

    feat: 인접한 지뢰 개수 계산

--- src/main/kotlin/minesweeper/domain/Board.kt
+++ src/main/kotlin/minesweeper/domain/Board.kt
@@ -1,18 +1,50 @@
 package minesweeper.domain
 
+import Mines
+
 class Board private constructor(
     val size: BoardSize,
     val mines: Mines,
+    private val cells: Map<Position, Cell>
 ) {
+    fun getCell(position: Position): Cell? = cells[position]
+
     companion object {
+
         fun create(
             rows: Int,
             columns: Int,
             mineCount: Int,
         ): Board {
             val boardSize = BoardSize(rows, columns)
+            require(mineCount < rows * columns) { "지뢰 개수는 보드 크기보다 작아야 합니다." }
+
             val mines = Mines.create(boardSize, MineCount(mineCount))
-            return Board(boardSize, mines)
+            val cells = createCells(boardSize, mines)
+            return Board(boardSize, mines, cells)
+        }
+
+        private fun createCells(boardSize: BoardSize, mines: Mines): Map<Position, Cell> {
+            val adjacentMineCounts = mutableMapOf<Position, Int>()
+
+            for (minePosition in mines.positions.values) {
+                minePosition.getAdjacentPositions(boardSize).forEach { position ->
+                    adjacentMineCounts[position] = adjacentMineCounts.getOrDefault(position, 0) + 1
+                }
+            }
+
+            return buildMap {
+                for (row in 0 until boardSize.rows) {
+                    for (col in 0 until boardSize.columns) {
+                        val position = Position(row, col)
+                        if (position in mines) {
+                            put(position, Cell.Mine(position))
+                        } else {
+                            put(position, Cell.Empty(position, adjacentMineCounts.getOrDefault(position, 0)))
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- src/main/kotlin/minesweeper/domain/Position.kt
+++ src/main/kotlin/minesweeper/domain/Position.kt
@@ -5,4 +5,17 @@ data class Position(val row: Int, val column: Int) {
         require(row >= 0) { "행은 0 이상이어야 합니다." }
         require(column >= 0) { "열은 0 이상이어야 합니다." }
     }
+
+    fun getAdjacentPositions(boardSize: BoardSize): List<Position> {
+        return Direction.entries
+            .mapNotNull {
+                val adjacent = it.getAdjacentPosition(this)
+                if (adjacent.isValid(boardSize)) adjacent else null
+            }
+    }
+
+    private fun isValid(boardSize: BoardSize): Boolean {
+        return row in 0 until boardSize.rows &&
+                column in 0 until boardSize.columns
+    }
 }
